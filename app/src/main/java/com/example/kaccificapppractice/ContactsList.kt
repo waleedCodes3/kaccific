0b440b47f65e44634314be31f73656ec2542ee73
waleedCodes3 <waleedcodes03@gmail.com>
2023-10-20 18:29:31 +0500

    contactListPageStarted

---
+++ app/src/main/java/com/example/kaccificapppractice/ContactsList.kt
@@ -0,0 +1,11 @@
+package com.example.kaccificapppractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactsList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contacts_list)
+    }
+}
---
+++ app/src/main/java/com/example/kaccificapppractice/contactCard.kt
@@ -0,0 +1,11 @@
+package com.example.kaccificapppractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class contactCard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_card)
+    }
+}
